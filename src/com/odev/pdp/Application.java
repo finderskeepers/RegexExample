e913b1f9230a58ecbda4f83869461d57f3151ab5
finderskeepers <burak.guleryuz@ogr.sakarya.edu.tr>
2015-04-18 14:54:26 +0300

    added declarations

--- src/com/odev/pdp/Application.java
+++ src/com/odev/pdp/Application.java
@@ -8,13 +8,13 @@ import java.util.regex.Pattern;
 
 public class Application {
 
-	public String[] readFromText() throws FileNotFoundException{
+	public String[] readFromText() throws FileNotFoundException{ //reads lines from the text, stores them in lines array
 		
 		Scanner input = new Scanner(new File("veriler.txt"));
 		String[] lines = new String[10];
 		int count = 0;
 		
-		while(input.hasNext()){
+		while(input.hasNext()){					//if there is another line, stores it in the lines array
 			lines[count] = input.nextLine();
 			count++;
 		}
@@ -27,41 +27,42 @@ public class Application {
 	
 	public boolean isProper(String line){
 		
-		String pattern = "sayilar(([0-9]+\\s)+)([0-9]+#)";
+		String pattern = "sayilar(([0-9]+\\s)+)([0-9]+#)";	//format is like : sayilar12 24 34#
 		
-		Pattern pt = Pattern.compile(pattern);
+		Pattern pt = Pattern.compile(pattern);				//compiles defined string(your regex) into a pattern
 		
-		Matcher match = pt.matcher(line);
+		Matcher match = pt.matcher(line);					//scans the line you give
 		
-		if(match.find())
+		if(match.find())									//if finds any match, returns true
 			return true;
 		
-		
-		else
+		else												//if not, returns false
 			return false;
 	}
 	
 	
-	public String[] parsing(String[] lines){
-		
-		String[] numbers = new String[30];
+	public String[] parsing(String[] lines){				//splits the proper lines into numbers and
+															//stores it in the numbers string
+		String[] numbers = new String[30];				
 		int countForLines = 0;
 		int count = 0;
 		
-		while(lines[countForLines]!=null){
+		while(lines[countForLines]!=null){					//if line is not null
 		
-			if(isProper(lines[countForLines])){
-				Pattern pt = Pattern.compile("[0-9]{2,}+");
-				Matcher match = pt.matcher(lines[countForLines]);
+			if(isProper(lines[countForLines])){				//if line is proper according to you regex design
+				Pattern pt = Pattern.compile("[0-9]{2,}+");			//finds the numbers
+				Matcher match = pt.matcher(lines[countForLines]);	//scans the line
 			
-				while(match.find()){
-					System.out.println(match.group());
+				while(match.find()){						//if finds any number
+					System.out.println(match.group());		//stores it in the numbers array
 					numbers[count] = match.group();
 					count++;
 				}
 			}
-			else
+			
+			else											//if line is not proper to your regex design :
 				System.out.println(lines[countForLines] + " is not proper!...");
+			
 			countForLines++;
 	    }
 			
@@ -69,21 +70,21 @@ public class Application {
 	}
 	
 	
-	public double harmonicalAverage(String[] array){
+	public double harmonicalAverage(String[] array){	//makes harmonical average of founded numbers in the txt
 		
 		double average = 0;
-		double count = 0;
+		double count = 0;		//count is for how many numbers you have
 		
 		for(int i = 0; i < array.length; i++){
 			if(array[i]!=null){
-			average += 1 / Double.parseDouble(array[i]);
-			count++;
+			average += 1 / Double.parseDouble(array[i]); //double.parsedouble is a transforming method
+			count++;											//that transforms string to double
 			}
 		}
 		
 		average = count / average;
 		
-		return (int)average;
+		return (int)average;		//clearing the floating part
 	}
 	
 }
