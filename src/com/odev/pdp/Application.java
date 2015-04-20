1f7c1dd5b3bd0097f3e36f7577d9c370f6b0c991
finderskeepers <burak.guleryuz@ogr.sakarya.edu.tr>
2015-04-20 14:00:12 +0300

    Redesigned declarations

--- src/com/odev/pdp/Application.java
+++ src/com/odev/pdp/Application.java
@@ -8,82 +8,83 @@ import java.util.regex.Pattern;
 
 public class Application {
 
-	public String[] readFromText() throws FileNotFoundException{ //reads lines from the text, stores them in lines array
-		
+	public String[] readFromText() throws FileNotFoundException { // reads lines from the text,
+															 //stores them in lines array
 		Scanner input = new Scanner(new File("veriler.txt"));
 		String[] lines = new String[10];
 		int count = 0;
-		
-		while(input.hasNext()){			//if there is another line, stores it in the lines array
+
+		while (input.hasNext()) { // if there is another line, stores it in the lines array
 			lines[count] = input.nextLine();
 			count++;
 		}
-		
+
 		input.close();
-		
+
 		return lines;
 	}
-	
-	
-	public boolean isProper(String line){
-		
-		String pattern = "sayilar(([0-9]+\\s)+)([0-9]+#)";	//format is like : sayilar12 24 34#
-		
-		Pattern pt = Pattern.compile(pattern);			//compiles defined string(your regex) into a pattern
-		
-		Matcher match = pt.matcher(line);				//scans the line you give
-		
-		if(match.find())	//if finds any match, returns true
+
+	public boolean isProper(String line) {
+
+		String pattern = "sayilar(([0-9]+\\s)+)([0-9]+#)"; // format is like :
+															// sayilar12 24 34#
+
+		Pattern pt = Pattern.compile(pattern); // compiles defined string(your
+												// regex) into a pattern
+
+		Matcher match = pt.matcher(line); // scans the line you give
+
+		if (match.find()) // if finds any match, returns true
 			return true;
-		
-		else	//if not, returns false
+
+		else			  // if not, returns false
 			return false;
 	}
-	
-	
-	public String[] parsing(String[] lines){				//splits the proper lines into numbers and
-															//stores it in the numbers string
-		String[] numbers = new String[30];				
+
+	public String[] parsing(String[] lines) { // splits the proper lines into
+												// numbers and
+		String[] numbers = new String[30]; //stores it in the numbers string array
 		int countForLines = 0;
 		int count = 0;
-		
-		while(lines[countForLines]!=null){					//if line is not null
-		
-			if(isProper(lines[countForLines])){				//if line is proper according to you regex design
-				Pattern pt = Pattern.compile("[0-9]+");			//finds the numbers
-				Matcher match = pt.matcher(lines[countForLines]);	//scans the line
-			
-				while(match.find()){						//if finds any number		
-					numbers[count] = match.group();			//stores it in the numbers array
+
+		while (lines[countForLines] != null) { // if line is not null
+
+			if (isProper(lines[countForLines])) { // if line is proper according
+													// to you regex design
+				Pattern pt = Pattern.compile("[0-9]+"); // finds the numbers
+				Matcher match = pt.matcher(lines[countForLines]); // scans the line
+
+				while (match.find()) { // if finds any number
+					numbers[count] = match.group(); // stores it in the numbers array
 					count++;
 				}
 			}
-			
-			else		//if line is not proper to your regex design :
+
+			else	// if line is not proper to your regex design :
 				System.out.println(lines[countForLines] + " is not proper!...");
-			
+
 			countForLines++;
-	    }
-			
+		}
+
 		return numbers;
 	}
-	
-	
-	public double harmonicalAverage(String[] array){	//makes harmonical average of founded numbers in the txt
-		
-		double average = 0;
-		double count = 0;		//count is for how many numbers you have
-		
-		for(int i = 0; i < array.length; i++){
-			if(array[i]!=null){
-			average += 1 / Double.parseDouble(array[i]); //double.parsedouble is a transforming method
-			count++;								//that transforms string to double
+
+	public double harmonicalAverage(String[] array) { // makes harmonical
+														// average of founded
+		double average = 0;							  //numbers in the txt
+		double count = 0; // count is for how many numbers you have
+
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] != null) {
+				average += 1 / Double.parseDouble(array[i]); // double.parsedouble
+															//is a transforming method
+				count++; 							// that transforms string to double
 			}
 		}
-		
+
 		average = count / average;
-		
-		return (int)average;	//clearing the floating part
+
+		return (int) average; // clearing the floating part
 	}
-	
+
 }
