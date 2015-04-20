5996e66615a802ce461e33a72c9c32779e576c2a
finderskeepers <burak.guleryuz@ogr.sakarya.edu.tr>
2015-04-20 14:05:59 +0300

    redesigned declarations

--- src/com/odev/pdp/Application.java
+++ src/com/odev/pdp/Application.java
@@ -9,8 +9,7 @@ import java.util.regex.Pattern;
 public class Application {
 
 	public String[] readFromText() throws FileNotFoundException { // reads lines from the text,
-															 //stores them in lines array
-		Scanner input = new Scanner(new File("veriler.txt"));
+		Scanner input = new Scanner(new File("veriler.txt"));	//stores them in the lines array
 		String[] lines = new String[10];
 		int count = 0;
 
@@ -26,11 +25,9 @@ public class Application {
 
 	public boolean isProper(String line) {
 
-		String pattern = "sayilar(([0-9]+\\s)+)([0-9]+#)"; // format is like :
-															// sayilar12 24 34#
-
-		Pattern pt = Pattern.compile(pattern); // compiles defined string(your
-												// regex) into a pattern
+		String pattern = "sayilar(([0-9]+\\s)+)([0-9]+#)"; // format is like : sayilar12 24 34#
+		
+		Pattern pt = Pattern.compile(pattern); // compiles defined string(your regex) into a pattern
 
 		Matcher match = pt.matcher(line); // scans the line you give
 
@@ -41,16 +38,14 @@ public class Application {
 			return false;
 	}
 
-	public String[] parsing(String[] lines) { // splits the proper lines into
-												// numbers and
+	public String[] parsing(String[] lines) { // splits the proper lines into numbers and
 		String[] numbers = new String[30]; //stores it in the numbers string array
 		int countForLines = 0;
 		int count = 0;
 
 		while (lines[countForLines] != null) { // if line is not null
 
-			if (isProper(lines[countForLines])) { // if line is proper according
-													// to you regex design
+			if (isProper(lines[countForLines])) { // if line is proper according to you regex design
 				Pattern pt = Pattern.compile("[0-9]+"); // finds the numbers
 				Matcher match = pt.matcher(lines[countForLines]); // scans the line
 
@@ -69,16 +64,14 @@ public class Application {
 		return numbers;
 	}
 
-	public double harmonicalAverage(String[] array) { // makes harmonical
-														// average of founded
+	public double harmonicalAverage(String[] array) { // makes harmonical average of founded
 		double average = 0;							  //numbers in the txt
-		double count = 0; // count is for how many numbers you have
+		double count = 0; //count is for how many numbers you have
 
 		for (int i = 0; i < array.length; i++) {
 			if (array[i] != null) {
 				average += 1 / Double.parseDouble(array[i]); // double.parsedouble
-															//is a transforming method
-				count++; 							// that transforms string to double
+				count++; 			//is a transforming method that transforms string to double
 			}
 		}
 
